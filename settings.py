a0c8f98eadaf343ce81d64223b777d4a9f598e0e
Adriana <adrisheu@Adrianas-MacBook-Pro.local>
2024-02-15 11:01:09 -0700

    Update documents based off of the changes made during the hackathon

--- settings.py
+++ settings.py
@@ -1,3 +1,10 @@
+"""
+Settings File to place API_key or any other variables that will change between users. It also allows us to change
+easier across all files as well. 
+    
+"""
 
-
-API_KEY = ""
+API_KEY = "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"
+LIBRARY_ID = "4530692"
+LIBRARY_TYPE = "group"
+ZOLTERO_KEY ="vdtxtnjWsMBMBIrWgfF4i2CY"
